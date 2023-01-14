f17f2675ca2a63c38179fd11c2d2635f92dcc5eb
EmilyF99 <emilyfletcher099@gmail.com>
2023-01-14 15:15:11 +0000

    Articles can be added and viewed

--- websites/default/foot.php
+++ websites/default/foot.php
@@ -1,7 +1,7 @@
 <!--Template file, used to make the footer and for the html layouts closing tags-->
 </main>
 		<footer>
-			&copy; Northampton News 2021
+			&copy; Fotheby's 2022
 		</footer>
 	</body>
 </html>
--- websites/default/head.php
+++ websites/default/head.php
@@ -2,7 +2,8 @@
 <!-- Template file for loadinf the top of the website layout and opening tags-->
 <html lang = "en">
 	<head>
-		<link rel="stylesheet" href="styles.css"/>
+		<link rel="stylesheet" href="../styles.css"/>
+		
 		<!--This variable is so each page can have a unique title, makes navigation easier -->
 		<title>
 			<?php echo $title ?>
@@ -12,6 +13,6 @@
 		<header>
 			<!--Used to generate the top banner above the Nav bar-->
 			<section>
-				<h1>Northampton News</h1>
+				<h1>Fotheby's</h1>
 			</section>
 		</header>
